908aea76881e5e3870e1fb9359a9c9644ed06dbc
Preston Pham <prestonp08@gmail.com>
2013-01-13 04:07:07 -0600

    added color swatches

--- public/js/main.js
+++ public/js/main.js
@@ -6,6 +6,7 @@ ctx.fillRect(30, 30, 50, 50);
 
 
 $(function() {
+  /* Set up jQueryUI sliders */
   $('#size-slider').slider({
     value: 50,
     range: 'min',
@@ -18,4 +19,54 @@ $(function() {
   });
 });
 
-//$('drawwwwCanvas').append("ppppp");
+$(document).ready(function() {
+  var $currswatch = null;
+
+  /**
+    Convert rgb or rgba values 
+    to hexadecimal
+  **/
+  function rgba2hex(str) {
+    str = str.indexOf("rgba") !== -1 ?
+            str.match(/^rgba\((\d+),\s*(\d+),\s*(\d+),\s*(\d+)\)$/) : 
+            str.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+    function hex(x) {
+      return ("0" + parseInt(x).toString(16)).slice(-2);
+    }
+    return "#" + hex(str[1]) + hex(str[2]) + hex(str[3]);
+  }
+
+  /**
+    Double click swatches to open
+    color picker
+  **/  
+  $('.swatch').live('dblclick', function() {
+    var $curr = $(this);
+    var $hex = rgba2hex($curr.css('background-color'));
+    $('#colorModal').modal('show');
+    $('#color').attr('value', $hex);
+    $('#colorpicker').farbtastic('#color');
+    $currswatch = $curr;
+  });
+
+  /**
+    Click a swatch to set
+    current color
+  **/
+  $('.swatch').live('click', function() {
+    var $curr = $(this);
+    var $hex = rgba2hex($curr.css('background-color'));
+    $('.current-color').css('background-color', $hex);
+  });
+
+  /** 
+    Set color after using
+    the color picker
+  **/
+  $('#set-color').click(function() {
+    var $hex = $('#color').attr('value');
+    $currswatch.css('background-color', $hex);
+    $('#colorModal').modal('hide');
+  }); 
+ 
+});
